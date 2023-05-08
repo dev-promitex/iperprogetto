40ba324a79c123df556c67246c96d29bec329e09
francescoarenapromitex <francescoarenapromitex@gmail.com>
2023-05-08 18:20:15 +0200

    structure

---
+++ Iperprogetto.php
@@ -0,0 +1,15 @@
+<?php
+/**
+* Plugin Name: IperProgetto
+* Plugin URI: https://www.iperprogetto.it/
+* Description: Marketplace di iperprogetto.
+* Version: 1.0
+* Author: Promitex S.R.L.
+* Author URI: https://www.promitex.it/
+**/
+
+function main() {
+    echo '<p>Questo è il mio primo plugin WordPress!</p>';
+}
+
+add_action( 'wp_footer', 'main' );
